99e6adcae7bed9140d32ebb5aa6526b5f2d21f1d
Shuang <shuangliu12@gmail.com>
2014-09-08 17:56:20 -0400

    first commit

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require_relative 'models/contact'
+
+# before do
+#   contact_attributes = [
+#     { first_name: 'Eric', last_name: 'Kelly', phone_number: '1234567890' },
+#     { first_name: 'Adam', last_name: 'Sheehan', phone_number: '1234567890' },
+#     { first_name: 'Dan', last_name: 'Pickett', phone_number: '1234567890' },
+#     { first_name: 'Evan', last_name: 'Charles', phone_number: '1234567890' },
+#     { first_name: 'Faizaan', last_name: 'Shamsi', phone_number: '1234567890' },
+#     { first_name: 'Helen', last_name: 'Hood', phone_number: '1234567890' },
+#     { first_name: 'Corinne', last_name: 'Babel', phone_number: '1234567890' }
+#   ]
+
+#   @contacts = contact_attributes.map do |attr|
+#     Contact.new(attr)
+#   end
+# end
+
+get '/' do
+  @page = params[:page].to_i || 0
+  @contacts = Contact.limit(1).offset(@page)
+  # @query = params[:query]
+  @results = Contact.where("first_name = ?", params[:query])
+  erb :index
+end
+
+get '/contacts/:id' do
+  id = params[:id]
+  @each_contact = Contact.find(id)
+  erb :show
+end
---
+++ models/contact.rb
@@ -0,0 +1,20 @@
+# class Contact
+#   attr_reader :first_name, :last_name, :phone_number
+
+#   def initialize(attributes)
+#     @first_name = attributes[:first_name]
+#     @last_name = attributes[:last_name]
+#     @phone_number = attributes[:phone_number]
+#   end
+
+#   def name
+#     [first_name, last_name].join(' ')
+#   end
+# end
+
+class Contact < ActiveRecord::Base
+  def name
+    [first_name, last_name].join(' ')
+  end
+
+end
